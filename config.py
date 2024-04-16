0c6846aa8a3344d1041a0e0be95aef14f2e5f881
ONGJW <151889868+ongjw0131@users.noreply.github.com>
2024-04-16 12:51:12 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-2.chac04say3j8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "aacs3253"
+customdb = "employee"
+custombucket = "ongjw-employee"
+customregion = "us-east-1"
